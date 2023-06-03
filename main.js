257719e92f8bbfc49ccc16921702d143fc0d5dea
Goutam Konapala <goutam.iitbbs@gmail.com>
2023-06-03 00:17:18 -0400

    second commit

--- main.js
+++ main.js
@@ -1,48 +1,104 @@
 import './style.css'
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
-const provider = new OpenStreetMapProvider();
+const provider = new OpenStreetMapProvider({
+  params: {
+    email: 'goutam.iitbbs@gmail.com',
+    searchText: 'nominatim',
+    delay: 500, // Delay in milliseconds between consecutive API calls
+  },
+});
+
+let debounceTimeout;
 
 const searchInput = document.getElementById('address-input');
 searchInput.addEventListener('input', handleAddressInput);
 
+const suggestionsContainer = document.getElementById('address-suggestions');
+const resultContainer = document.getElementById('result-container');
+const latitudeElement = document.getElementById('latitude');
+const longitudeElement = document.getElementById('longitude');
 function handleAddressInput(event) {
-  const query = event.target.value;
+  clearTimeout(debounceTimeout);
 
+  const query = event.target.value.trim();
+
+  if (query === '') {
+    hideAddressSuggestions();
+    clearAddressSuggestions();
+    hideResult();
+    return;
+  }
+
+  debounceTimeout = setTimeout(() => {
   provider.search({ query })
     .then((results) => {
       // Clear previous suggestions
       clearAddressSuggestions();
 
-      // Display new suggestions
-      results.forEach((result) => {
-        const suggestion = document.createElement('div');
-        suggestion.classList.add('address-suggestion');
-        suggestion.textContent = result.label;
-        suggestion.addEventListener('click', () => handleAddressSelection(result));
+      if (results.length === 0) {
+        hideAddressSuggestions();
+        hideResult();
 
-        const suggestionsContainer = document.getElementById('address-suggestions');
-        suggestionsContainer.appendChild(suggestion);
-      });
-    })
-    .catch((error) => {
-      console.error('Error retrieving address suggestions:', error);
-    });
-}
+        return;
+      }
 
-function handleAddressSelection(result) {
-  const lat = result.y;
-  const lng = result.x;
+          // Display new suggestions
+          results.forEach((result) => {
+            const suggestion = document.createElement('div');
+            suggestion.classList.add('address-suggestion');
+            suggestion.textContent = result.label;
+            suggestion.addEventListener('click', () => handleAddressSelection(result));
 
-  console.log('Latitude:', lat);
-  console.log('Longitude:', lng);
-}
+            suggestionsContainer.appendChild(suggestion);
+          });
 
-function clearAddressSuggestions() {
-  const suggestionsContainer = document.getElementById('address-suggestions');
-  if (suggestionsContainer) {
-    while (suggestionsContainer.firstChild) {
-      suggestionsContainer.removeChild(suggestionsContainer.firstChild);
-    }
+          showAddressSuggestions();
+
+        })
+        .catch((error) => {
+          console.error('Error retrieving address suggestions:', error);
+        });
+    }, 300);
   }
-}
+
+    function handleAddressSelection(result) {
+      const selectedAddress = result.label;
+      const lat = result.y;
+      const lng = result.x;
+
+      searchInput.value = selectedAddress;
+      hideAddressSuggestions();
+      clearAddressSuggestions();
+      showResult(lat, lng);
+
+
+      console.log('Latitude:', lat);
+      console.log('Longitude:', lng);
+    }
+
+    function showAddressSuggestions() {
+      suggestionsContainer.style.display = 'block';
+    }
+
+    function hideAddressSuggestions() {
+      suggestionsContainer.style.display = 'none';
+    }
+
+    function clearAddressSuggestions() {
+      while (suggestionsContainer.firstChild) {
+        suggestionsContainer.removeChild(suggestionsContainer.firstChild);
+      }
+    }
+    function showResult(lat, lng) {
+      latitudeElement.textContent = `Latitude: ${lat}`;
+      longitudeElement.textContent = `Longitude: ${lng}`;
+      resultContainer.style.display = 'block';
+    }
+
+    function hideResult() {
+      resultContainer.style.display = 'none';
+    }
+    if (searchInput.value.trim() === '') {
+      hideAddressSuggestions();
+    }
